2dbae367ef8aefe72ccbbcae14d939971714ddbd
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-02-21 12:43:33 +0530

    Task-17: Reports feature frontend.

---
+++ public/js/reports.js
@@ -0,0 +1,36 @@
+/* /public/js/reports.js */
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const dailyReportBtn = document.getElementById('dailyReportBtn');
+//     const monthlyReportBtn = document.getElementById('monthlyReportBtn');
+//     const yearlyReportBtn = document.getElementById('yearlyReportBtn');
+
+//     const dailyTable = document.getElementById('dailyTable');
+//     const monthlyTable = document.getElementById('monthlyTable');
+//     const yearlyTable = document.getElementById('yearlyTable');
+
+//     const reportButtons = document.getElementById('reportButtons');
+
+//     dailyReportBtn.addEventListener('click', () => {
+//         dailyTable.style.display = 'block';
+//         monthlyTable.style.display = 'none';
+//         yearlyTable.style.display = 'none';
+//     });
+
+//     monthlyReportBtn.addEventListener('click', () => {
+//         dailyTable.style.display = 'none';
+//         monthlyTable.style.display = 'block';
+//         yearlyTable.style.display = 'none';
+//     });
+
+//     yearlyReportBtn.addEventListener('click', () => {
+//         dailyTable.style.display = 'none';
+//         monthlyTable.style.display = 'none';
+//         yearlyTable.style.display = 'block';
+//     });
+
+//     reportButtons.style.display = 'flex';
+//     dailyTable.style.display = 'none';
+//     monthlyTable.style.display = 'none';
+//     yearlyTable.style.display = 'none';
+// });
