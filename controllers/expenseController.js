72f67c00368ec730aca9ff2c227069b8eaae85d0
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-03-01 12:36:06 +0530

    Task18 : Added Pagination.

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -164,7 +164,7 @@ exports.getHomePage = async (req, res, next) => {
   exports.getAllExpensesforPagination = async (req, res, next) => {
     try {
       const pageNo = req.params.page;
-      const limit = 10;
+      const limit = 5;
       const offset = (pageNo - 1) * limit;
       const totalExpenses = await Expense.count({
         where: { userId: req.user.id },
