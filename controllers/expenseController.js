37fc686617533c331b5e9e3f9fd33ceb916e4c95
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-03-01 12:10:01 +0530

    Making code for AWS Services but its not functional right now.

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -5,7 +5,7 @@ const Expense = require("../models/expenseModel");
 const database = require("../util/database");
 const User = require("../models/userModel");
 const sequelize = require('../util/database');
-
+const AWS = require("aws-sdk");
 exports.getHomePage = async (req, res, next) => {
   try {
     res.sendFile(
@@ -112,6 +112,70 @@ exports.getHomePage = async (req, res, next) => {
       );
   
       res.redirect("/homePage");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+//S3 Services for download and file uploads
+  exports.uploadToS3 = (data, filename) => {
+    const BUCKET_NAME = "spendzi-expense-tracker-reports";
+    const IAM_USER_KEY = "AKIA5D6VUZQBKFDKYUNS";
+    const IAM_USER_SECRET = "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";
+  
+    AWS.config.update({
+      accessKeyId: IAM_USER_KEY,
+      secretAccessKey: IAM_USER_SECRET,
+    });
+  
+    const s3bucket = new AWS.S3({ params: { Bucket: BUCKET_NAME } });
+  
+    s3bucket.createBucket(() => {
+      const params = {
+        Bucket: BUCKET_NAME,
+        Key: filename,
+        Body: data,
+      };
+  
+      s3bucket.upload(params, (err, s3response) => {
+        if (err) {
+          console.log('Something went wrong.', err);
+        } else {
+          console.log('success', s3response);
+        }
+      });
+    });
+  };
+  
+  exports.downloadExpense = async (req, res) => {
+    try {
+      const expenses = await Expense.findAll({ where: { userId: req.user.id } });
+      const stringifiedExpenses = JSON.stringify(expenses);
+      const filename = "Expense.txt";
+      exports.uploadToS3(stringifiedExpenses, filename);
+      res.status(200).json({ success: true });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ success: false, error: err.message });
+    }
+  };
+
+  //for pagination
+  exports.getAllExpensesforPagination = async (req, res, next) => {
+    try {
+      const pageNo = req.params.page;
+      const limit = 10;
+      const offset = (pageNo - 1) * limit;
+      const totalExpenses = await Expense.count({
+        where: { userId: req.user.id },
+      });
+      const totalPages = Math.ceil(totalExpenses / limit);
+      const expenses = await Expense.findAll({
+        where: { userId: req.user.id },
+        offset: offset,
+        limit: limit,
+      });
+      res.json({ expenses: expenses, totalPages: totalPages });
     } catch (err) {
       console.log(err);
     }
