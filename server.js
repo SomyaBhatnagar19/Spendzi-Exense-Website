7662c7d9d032b165635df6525457e431aeb84577
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-03-16 12:33:06 +0530

    Deployment starting..

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ const morgan = require("morgan");
 
 const app = express();
 
+const cors = require("cors");
+app.use(cors());
+
+const dotenv = require("dotenv");
+dotenv.config();
+
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, "access.log"),
   { flags: "a" }
@@ -37,6 +43,7 @@ const leaderboardRouter = require("./router/leaderboardRouter");
 const resetPasswordRouter = require("./router/resetPasswordRouter");
 const reportsRouter = require("./router/reportsRouter");
 const creditExpenseRouter = require("./router/creditExpenseRouter");
+const downloadReportHistory = require("./router/downloadReportHistory");
 
 //for login & signup route
 app.use("/", userRouter);
@@ -49,6 +56,7 @@ app.use("/premium", leaderboardRouter);
 app.use("/password", resetPasswordRouter);
 app.use("/reports", reportsRouter);
 app.use("/credit", creditExpenseRouter);
+app.use("/history", downloadReportHistory);
 
 //Associations
 const User = require("./models/userModel");
@@ -56,6 +64,7 @@ const Expense = require('./models/expenseModel');
 const Order = require("./models/ordersModel");
 const ResetPassword = require("./models/resetPasswordModel");
 const CreditExpense = require("./models/creditExpenseModel");
+const History = require("./models/downloadReportHistory");
 
 User.hasMany(Expense, { foreignKey: 'userId' });
 Expense.belongsTo(User, { foreignKey: 'userId' });
@@ -68,11 +77,16 @@ User.hasMany(ResetPassword, { foreignKey: 'userId' });
 
 CreditExpense.belongsTo(User, { foreignKey: 'userId' });
 User.hasMany(CreditExpense, { foreignKey: 'userId' });
-// Start the server
 
+History.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(History, { foreignKey: 'userId' });
+
+// Start the server
 sequelize.sync().then(() => {
-  app.listen(3000, () => {
-    console.log(`Server is listening on port 3000.`);
+  const PORT = process.env.PORT || 3000;
+  // console.log(`${process.env.PORT}`);
+  app.listen(PORT , () => {
+    console.log(`Server is listening on port ${PORT}.`);
   });
 }).catch((error) => {
   console.error("Unable to connect to the database:", error);
